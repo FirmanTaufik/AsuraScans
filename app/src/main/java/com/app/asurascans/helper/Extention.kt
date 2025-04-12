9efc7a43211caec883286bbc3de8bae58a5497d7
FirmanTaufik <fkopitezz@gmail.com>
2025-04-12 08:22:06 +0700

    update : helper extention

--- app/src/main/java/com/app/asurascans/helper/Extention.kt
+++ app/src/main/java/com/app/asurascans/helper/Extention.kt
@@ -4,13 +4,34 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
-inline fun <reified T : Activity> Context.startNewActivity(
+inline fun <reified T : Activity> Activity.startNewActivity(
     extras: Bundle? = null,
-    flags: Int? = Intent.FLAG_ACTIVITY_NEW_TASK
+    flags: Int? = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT,
+    requestCode :Int? = 100
+
 ) {
     val intent = Intent(this, T::class.java)
     extras?.let { intent.putExtras(it) }
     flags?.let { intent.flags = it }
-    startActivity(intent)
+    if (requestCode != null) {
+        startActivityForResult(intent, requestCode)
+    }
+}
+
+fun <T> T.fromObjectToJson(): String = try {
+    Gson().toJson(this)
+} catch (e: Exception) {
+    ""
+}
+
+inline fun <reified T> String.fromJsonToObject(): T? {
+    return try {
+        val type = object : TypeToken<T>() {}.type
+        Gson().fromJson<T>(this, type)
+    } catch (e: Exception) {
+        null
+    }
 }
